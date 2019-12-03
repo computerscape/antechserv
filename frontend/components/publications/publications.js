0cc0a03ce833e0e5e67e6ddc09644f8502c2d134
Tom Chesnutt <tom@antechserv.com>
2019-12-02 16:17:50 -0800

    ElderNoKnothing
    swapped files so description matched the heading

--- frontend/components/publications/publications.js
+++ frontend/components/publications/publications.js
@@ -6,13 +6,14 @@ const Publications = [{
 },{
 	link_url: 'http://www.financingsustainablewater.org/tools/building-better-water-rates-uncertain-world',
 	image: './assets/publication/BuildingBetterWaterRatesHandbookCoverPicSmall.png',
-	header: 'Orange County Water Use Efficiency Master Plan (June 2013)',
-	text: 'This plan was developed by the Municipal Water District of Orange County in coordination with its member agencies to help reach mandated water use reductions by 2020. It creates a strategy to meet specific reduction targets and offers customized tools to track performance and adapt to changing circumstances.',
+	header: 'Building Better Water Rates',
+	text: 'Water ratemaking is a complex analytical endeavor composed of many building blocks, each of which needs to be addressed before a utility can model and select a successful rate structure. Such critical steps as demand forecasting, carrying out a cost of service analysis, determining revenue requirements, and identifying objectives cannot be undervalued. Only after completing them can rate setters undertake the exercise — or art — of designing a rate structure and evaluating it against multiple scenarios to determine its effectiveness.',
+
 },{
 	link_url: 'https://www.usbr.gov/lc/socal/reports/MWDOCMasterPlan2013.pdf',
 	image: './assets/publication/MWDOC_WUE_MasterPlan2013.png',
-	header: 'Building Better Water Rates',
-	text: 'Water ratemaking is a complex analytical endeavor composed of many building blocks, each of which needs to be addressed before a utility can model and select a successful rate structure. Such critical steps as demand forecasting, carrying out a cost of service analysis, determining revenue requirements, and identifying objectives cannot be undervalued. Only after completing them can rate setters undertake the exercise — or art — of designing a rate structure and evaluating it against multiple scenarios to determine its effectiveness.',
-}];
+	header: 'Orange County Water Use Efficiency Master Plan (June 2013)',
+	text: 'This plan was developed by the Municipal Water District of Orange County in coordination with its member agencies to help reach mandated water use reductions by 2020. It creates a strategy to meet specific reduction targets and offers customized tools to track performance and adapt to changing circumstances.',
+	}];
 
 export default Publications;
